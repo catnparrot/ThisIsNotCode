ea2929dec891d5988511a2adb6b2c871be2f85f3
catnparrot <idiotpi3rrot@gmail.com>
2023-04-17 01:36:18 +0900

    NP0416

---
+++ dpm04/d16/I_C_M_02_Person.java
@@ -0,0 +1,10 @@
+package dpm04.d16;
+
+public class I_C_M_02_Person {
+    String name;	// 필드
+    int age; 		// 필드
+    
+    public void sayHello() { // 메소드
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
---
+++ dpm04/d16/Instance_Class_Method_1.java
@@ -0,0 +1,21 @@
+package dpm04.d16;
+
+/*
+인스턴스(Instance)는 클래스(Class)를 기반으로 생성된 객체(Object)를 말합니다.
+즉, 클래스는 객체를 생성하기 위한 틀(template)이고,
+인스턴스는 이 틀을 기반으로 생성된 구체적인 객체입니다.
+예를 들어, "Person" 이라는 클래스를 정의했다고 가정해봅시다.
+이 클래스는 "name"과 "age"라는 두 개의 필드와
+"sayHello"라는 메소드를 갖는 것으로 정의되었습니다.
+이제, "Person" 클래스를 기반으로 "John" 이라는 이름과
+20살이라는 값을 가지는 새로운 인스턴스를 생성하면,
+"John"은 "Person" 클래스를 기반으로 만들어진 구체적인 객체인 인스턴스가 됩니다.
+*/
+
+public class Instance_Class_Method_1 {
+	public static void main(String[] args) {
+		System.out.println("인스턴스(Instance)는 클래스(Class)를 기반으로 생성된 객체(Object)를 말합니다. 즉, 클래스는 객체를 생성하기 위한 틀(template)이고, 인스턴스는 이 틀을 기반으로 생성된 구체적인 객체입니다.\r\n"
+				+ "\r\n"
+				+ "예를 들어, \"Person\" 이라는 클래스를 정의했다고 가정해봅시다. 이 클래스는 \"name\"과 \"age\"라는 두 개의 필드와 \"sayHello\"라는 메소드를 갖는 것으로 정의되었습니다. 이제, \"Person\" 클래스를 기반으로 \"John\" 이라는 이름과 20살이라는 값을 가지는 새로운 인스턴스를 생성하면, \"John\"은 \"Person\" 클래스를 기반으로 만들어진 구체적인 객체인 인스턴스가 됩니다.");
+	}
+}
